0da9b685553bcb49b6433b74d365f94574e5a82d
Gunabalan <69943544+gunabalan-0411@users.noreply.github.com>
2024-05-10 22:45:23 +0530

    Update letterCombinations.py

--- String/letterCombinations.py
+++ String/letterCombinations.py
@@ -1,4 +1,5 @@
 '''
+	https://leetcode.com/problems/letter-combinations-of-a-phone-number/description/
 	Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent.
 
 	A mapping of digit to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.
