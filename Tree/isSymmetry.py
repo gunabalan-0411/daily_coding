434a08ec080fd2927690393744a36a7699e4f0cc
Gunabalan <69943544+gunabalan-0411@users.noreply.github.com>
2024-05-13 15:55:06 +0530

    Day 20 Morning Coding - Tree
    is symmetric

---
+++ Tree/isSymmetry.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+def isSummetric(root):
+    def issymmetry(left, right):
+        # Both came to the last end after passing equal condition
+        if not left and not right:
+            return True
+        if not left or not right or left.val != right.val:
+            return False
+        
+        # Same as "Same tree" problem but instead we are comparing the opposite ends
+        return (
+            issymmetry(left.left, right.right) and
+            issymmetry(left.right, right.left)
+        )
+    
+    return issymmetry(root.left, root.right)
+        
