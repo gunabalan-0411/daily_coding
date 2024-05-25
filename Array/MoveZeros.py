ffe774dc3121226ef4257b7ac4c3e3377c4cfdcc
Gunabalan <69943544+gunabalan-0411@users.noreply.github.com>
2024-05-25 23:00:43 +0530

    Day 31 Morning Coding
    Move zeros

---
+++ Array/MoveZeros.py
@@ -0,0 +1,9 @@
+def moveZeroes(nums) -> None:
+    zeroIndex = 0
+    for index in range(len(nums)):
+        if nums[index] != 0:
+            nums[zeroIndex] = nums[index]
+            zeroIndex += 1
+            
+    for index in range(zeroIndex, len(nums)):
+        nums[index] = 0
