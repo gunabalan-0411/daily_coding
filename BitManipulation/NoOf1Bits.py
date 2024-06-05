d7306a1af20e8b7d5bceb3b22f3e28ae13aac564
Gunabalan <69943544+gunabalan-0411@users.noreply.github.com>
2024-06-05 10:59:36 +0530

    Day 42 Morning Coding
    Bit Manipulation: Number of 1 Bits

---
+++ BitManipulation/NoOf1Bits.py
@@ -0,0 +1,17 @@
+def hammingWeight(n):
+    # 33 ms: my solution
+    # return str(bin(n))[2:].count('1')
+
+    # 35 ms: bit wise algorithm
+    # modulus of each bit by 2 gives the count
+    res = 0
+    # while n:
+    #     res += n % 2
+    #     n = n >> 1 
+    # return res
+
+    # 19 ms instead of iterate each bit, directly hit 1s
+    while n:
+        n &= (n -1)
+        res += 1
+    return res
