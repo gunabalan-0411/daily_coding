50522e1843b29a5bd7cfe99d9f10de77b6fda9eb
Gunabalan <69943544+gunabalan-0411@users.noreply.github.com>
2024-06-21 21:27:00 +0530

    Day 55 Night Coding
    String: Sort the people

---
+++ SortPeople.py
@@ -0,0 +1,5 @@
+class Solution:
+    def sortPeople(self, names: list[str], heights: list[int]) -> list[str]:
+        return [height for _, height in
+            sorted([(height, name) for name, height in zip(names, heights)], reverse=True)]
+               
