efdc04dfa484515506b8c4c06e790b9f0de28919
Gunabalan <69943544+gunabalan-0411@users.noreply.github.com>
2024-06-26 20:29:23 +0530

    Day 63 Morning Coding,  Day 60 Night Coding
    String: Rings and Rods  Day 31, String: Count asterisks

---
+++ String/CountAsterisks.py
@@ -0,0 +1,15 @@
+'''
+https://leetcode.com/problems/count-asterisks/
+'''
+class Solution:
+    def countAsterisks(self, s: str) -> int:
+        ans = 0
+        bars = 0
+
+        for c in s:
+            if c == '|':
+                bars += 1
+            elif c == '*' and bars % 2 == 0:
+                ans += 1
+
+        return ans
