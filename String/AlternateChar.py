bb05f400266a840adcc3be34597d4d9661ca6239
Gunabalan <69943544+gunabalan-0411@users.noreply.github.com>
2024-07-02 19:31:21 +0530

    Day 66 Night Coding
    String: alternative merge

---
+++ String/AlternateChar.py
@@ -0,0 +1,7 @@
+'''
+https://leetcode.com/problems/merge-strings-alternately/
+'''
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        return ''.join(a + b for a, b in zip_longest(word1, word2, fillvalue=''))
+        
