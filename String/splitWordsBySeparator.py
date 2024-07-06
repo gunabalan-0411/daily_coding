2a4bcad2768a1c8443aa3417e86ac60e8afa7231
Gunabalan <69943544+gunabalan-0411@users.noreply.github.com>
2024-07-06 22:49:09 +0530

    Day 73 Morning Coding, Day 70 Night Coding
    String: Split String by separator, String: Remove trailing zeros from string

---
+++ String/splitWordsBySeparator.py
@@ -0,0 +1,6 @@
+class Solution:
+    def splitWordsBySeparator(self, words: list[str], separator: str) -> list[str]:
+        return [splitWord
+            for word in words
+            for splitWord in word.split(separator)
+            if splitWord]
