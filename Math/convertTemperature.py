866517c0135c79fa15cdca7b5a73661125a68155
Gunabalan <69943544+gunabalan-0411@users.noreply.github.com>
2024-07-07 21:46:09 +0530

    Day 74 Morning Coding, Day 71 Night Coding
    Math: Smallest Even multiple, Math: Convert the temperature

---
+++ Math/convertTemperature.py
@@ -0,0 +1,7 @@
+'''
+https://leetcode.com/problems/convert-the-temperature/
+'''
+class Solution:
+    def convertTemperature(self, celsius: float) -> list[float]:
+        return [celsius + 273.15, celsius * 1.8 + 32]
+        
