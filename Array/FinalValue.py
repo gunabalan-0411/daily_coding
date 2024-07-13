f36fa00795b459d4d2e592c6dda420e5b332a1d9
Gunabalan <69943544+gunabalan-0411@users.noreply.github.com>
2024-07-13 21:16:59 +0530

    Day 80 Morning Coding, Day 77 Night Coding
    Array: Count Pairs whose sum is greater than target, Array: Final value of variable after operations

---
+++ Array/FinalValue.py
@@ -0,0 +1,5 @@
+from typing import List
+class Solution:
+    def finalValueAfterOperations(self, operations: List[str]) -> int:
+        return sum(op[1] == '+' or -1 for op in operations)
+        
