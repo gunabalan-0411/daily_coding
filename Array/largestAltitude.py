a14a6a64f61ac85318d681e13e078f4afc317927
Gunabalan <69943544+gunabalan-0411@users.noreply.github.com>
2024-07-16 22:25:27 +0530

    Day 83 Morning Coding, Day 80 Night Coding

---
+++ Array/largestAltitude.py
@@ -0,0 +1,10 @@
+from typing import List
+class Solution:
+    def largestAltitude(self, gain: List[int]) -> int:
+        ans = 0
+        currAltitude = 0
+        for g in gain:
+            currAltitude += g
+            ans = max(ans, currAltitude)
+        return ans
+        
