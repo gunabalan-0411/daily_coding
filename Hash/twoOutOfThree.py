94966d221329705fa2fa5bf26336d9dc01b53783
Gunabalan <69943544+gunabalan-0411@users.noreply.github.com>
2024-07-20 22:35:45 +0530

    Day 87 Morning Coding, Day 84 Night Coding

---
+++ Hash/twoOutOfThree.py
@@ -0,0 +1,9 @@
+from typing import List
+import collections
+class Solution:
+    def twoOutOfThree(self, nums1: List[int], nums2: List[int], nums3: List[int]) -> List[int]:
+        count = collections.Counter()
+        for nums in nums1, nums2, nums3:
+            count.update(set(nums))
+        return [i for i, c in count.items() if c >= 2]
+        
