fe8915a4cd0fe3321767ea7557593b1159f0a5cc
Gunabalan <69943544+gunabalan-0411@users.noreply.github.com>
2024-07-22 21:41:50 +0530

    Day 86 Night Coding

---
+++ String/isAcronym.py
@@ -0,0 +1,5 @@
+from typing import List
+class Solution:
+    def isAcronym(self, words: List[str], s: str) -> bool:
+        return len(words) == len(s) and all(word[0] == c for word, c in zip(words, s))
+        
