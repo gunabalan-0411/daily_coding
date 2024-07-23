dbbda0d562a8699bab239a7a3fa299b61376b093
Gunabalan <69943544+gunabalan-0411@users.noreply.github.com>
2024-07-23 21:44:50 +0530

    Day 90 Morning Coding, Day 87 Night Coding

---
+++ String/commonChars.py
@@ -0,0 +1,8 @@
+from typing import List
+import collections
+import functools
+class Solution:
+    def commonChars(self, words: List[str]) -> List[str]:
+        return functools.reduce(lambda a, b: a & b,
+                            map(collections.Counter, words)).elements()
+        
---
+++ String/countKeyChanges.py
@@ -0,0 +1,7 @@
+import itertools
+
+class Solution:
+    def countKeyChanges(self, s: str) -> int:
+        return sum(a.lower() != b.lower()
+               for a, b in itertools.pairwise(s))
+        
