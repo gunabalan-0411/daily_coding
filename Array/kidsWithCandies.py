75f65cee4b8a856677c3ccc0e51311319b5b4483
Gunabalan <69943544+gunabalan-0411@users.noreply.github.com>
2024-07-26 21:13:52 +0530

    Day 93 Morning Coding, Day 89 Night Coding

---
+++ Array/kidsWithCandies.py
@@ -0,0 +1,5 @@
+from typing import List
+class Solution:
+    def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
+        maxCandy = max(candies)
+        return [candy + extraCandies >= maxCandy for candy in candies]
