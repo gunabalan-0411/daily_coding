21cd88ddf1f2c268a206e18712d66d1efbe1555d
Gunabalan <69943544+gunabalan-0411@users.noreply.github.com>
2024-07-27 21:05:39 +0530

    Day 94 Morning Coding, Day 90 Night Coding,

---
+++ String/halvesAreAlike.py
@@ -0,0 +1,7 @@
+class Solution:
+    def halvesAreAlike(self, s: str) -> bool:
+        kVowels = 'aeiouAEIOU'
+        aVowelsCount = sum(c in kVowels for c in s[:len(s) // 2])
+        bVowelsCount = sum(c in kVowels for c in s[len(s) // 2:])
+        return aVowelsCount == bVowelsCount
+        
