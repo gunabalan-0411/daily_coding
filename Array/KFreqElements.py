8d7dd85df9dc6adf43aaad3087daf556e5876db3
Gunabalan <69943544+gunabalan-0411@users.noreply.github.com>
2024-08-05 10:48:03 +0530

    Day 99 Night Coding
    Array : Top K Frequent Elements

---
+++ Array/KFreqElements.py
@@ -0,0 +1,18 @@
+from typing import List
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        counts = {}
+        freq = [[] for _ in range(len(nums)+1)]
+
+        for num in nums:
+            counts[num] = counts.get(num,0)+1
+        
+        for num, count in counts.items():
+            freq[count].append(num)
+        res = []
+        for i in range(len(freq)-1, 0, -1):
+            for n in freq[i]:
+                res.append(n)
+                if len(res) == k:
+                    return res
+        
