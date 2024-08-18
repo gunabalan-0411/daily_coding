29623af1889f0c39adfee23cbd1c05bf0c3c1dae
Gunabalan <69943544+gunabalan-0411@users.noreply.github.com>
2024-08-18 21:46:15 +0530

    Day 116 Morning Coding, Day 112 Night Coding

---
+++ Array/canAliceWin.py
@@ -0,0 +1,7 @@
+from typing import List
+class Solution:
+    def canAliceWin(self, nums: List[int]) -> bool:
+        a = sum(x for x in nums if x < 10)
+        b = sum(x for x in nums if x > 9)
+        return a != b
+        
