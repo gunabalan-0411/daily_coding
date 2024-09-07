3892eccc4d92bf9611626807b385329177e487e6
Gunabalan <69943544+gunabalan-0411@users.noreply.github.com>
2024-09-07 23:57:17 +0530

    Day 136 Morning Coding

---
+++ Array/countLHS.py
@@ -0,0 +1,12 @@
+from typing import List
+import collections
+class Solution:
+    def findLHS(self, nums: List[int]) -> int:
+        ans = 0
+        count = collections.Counter(nums)
+
+        for num, freq in count.items():
+            if num + 1 in count:
+                ans = max(ans, freq + count[num + 1])
+
+        return ans
