af4fdc7a45774d3391a95f6c5cdf6893e7a1d3d0
Gunabalan <69943544+gunabalan-0411@users.noreply.github.com>
2024-09-11 21:35:31 +0530

    Day 140 Morning Coding

---
+++ LinkedList/detectCycle.py
@@ -0,0 +1,32 @@
+# Definition for singly-linked list.
+from typing import Optional
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        # cur = head
+        # visited = set()
+        # while cur:
+        #     if cur in visited:
+        #         return cur
+        #     else:
+        #         visited.add(cur)
+        #     cur = cur.next
+        # return None
+        slow = head
+        fast = head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                slow = head
+                while slow != fast:
+                    slow = slow.next
+                    fast = fast.next
+                return slow
+
+        return None
