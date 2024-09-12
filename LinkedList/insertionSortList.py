2b8904c7c061961d04ed6d164d239b0a9001fe2d
Gunabalan <69943544+gunabalan-0411@users.noreply.github.com>
2024-09-12 22:01:53 +0530

    Day 137 Night Coding

---
+++ LinkedList/insertionSortList.py
@@ -0,0 +1,22 @@
+# Definition for singly-linked list.
+from typing import Optional
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def insertionSortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode(0)
+        prev = dummy  # the last and thus largest of the sorted list
+
+        while head:  # the current inserting node
+            next = head.next  # Cache the next inserting node.
+            if prev.val >= head.val:
+                prev = dummy  # Move `prev` to the front.
+            while prev.next and prev.next.val < head.val:
+                prev = prev.next
+            head.next = prev.next
+            prev.next = head
+            head = next  # Update the current inserting node.
+
+        return dummy.next
