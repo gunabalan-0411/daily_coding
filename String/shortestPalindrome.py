78f275f70493b7cdd6ab349f1cdfae6e9144e03e
Gunabalan <69943544+gunabalan-0411@users.noreply.github.com>
2024-09-20 22:30:11 +0530

    Day 145 Night Coding

---
+++ String/shortestPalindrome.py
@@ -0,0 +1,13 @@
+class Solution:
+    def shortestPalindrome(self, s: str) -> str:
+        def ispali(s, l, r):
+            while l <= r:
+                if s[l] != s[r]:
+                    return False
+                l, r = l+1, r-1
+            return True
+        for r in reversed(range(len(s))):
+            if ispali(s, 0, r):
+                suffix = s[r+1:]
+                return suffix[::-1] + s
+        return ''
