be7305ba3c199fd08b4b538fcf40116d18c75875
Gunabalan <69943544+gunabalan-0411@users.noreply.github.com>
2024-09-24 22:03:09 +0530

    Day 149 Night Coding

---
+++ Hash/longestCommonPrefix.py
@@ -0,0 +1,18 @@
+from typing import List
+class Solution:
+    def longestCommonPrefix(self, arr1: List[int], arr2: List[int]) -> int:
+        if len(arr1) > len(arr2):
+            arr1, arr2 = arr2, arr1
+
+        visited = set()
+        for n in arr1:
+            while n and n not in visited:
+                visited.add(n)
+                n = n // 10
+        res = 0
+        for n in arr2:
+            while n and n not in visited:
+                n = n // 10
+            if n:
+                res = max(res, len(str(n)))
+        return res
