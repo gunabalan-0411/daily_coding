8b2ea57f8eda6b8e58afc3c8280c8066be9659da
Gunabalan <69943544+gunabalan-0411@users.noreply.github.com>
2024-10-02 22:49:54 +0530

    Day 161 Morning Coding

---
+++ Hash/arrayRankTransform.py
@@ -0,0 +1,10 @@
+from typing import List
+class Solution:
+    def arrayRankTransform(self, arr: List[int]) -> List[int]:
+        rank = {}
+
+        for a in sorted(arr):
+            if a not in rank:
+                rank[a] = len(rank) + 1
+
+        return map(rank.get, arr)
