7eb2e339120a1be146acf196af449260803f44e7
Gunabalan <69943544+gunabalan-0411@users.noreply.github.com>
2025-01-03 13:38:57 +0530

    Day #3 Routines
    Matrix: Search a 2D Matrix II

---
+++ Matrix/searchMatrix.py
@@ -0,0 +1,14 @@
+from typing import List
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        N, M = len(matrix), len(matrix[0])
+        r, c = N - 1, 0
+        while r >= 0 and c < M:
+            if matrix[r][c] == target:
+                return True
+            if matrix[r][c] < target:
+                c += 1
+            else:
+                r -= 1                
+        return False
+        
